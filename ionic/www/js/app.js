ab76841a76e8e8800e306072d78433d3732f9a7a
João Sales <joaosalless@gmail.com>
2015-11-16 10:40:12 -0200

    --amend

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -17,3 +17,14 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+    .config(function ($stateProvider) {
+      $stateProvider.state('home', {
+        url: '/home',
+        templateUrl: 'templates/home.html'
+      })
+      .state('main', {
+        url: '/',
+        templateUrl: 'templates/main.html'
+      })
+    });
