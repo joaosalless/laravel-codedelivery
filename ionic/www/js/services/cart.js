a5499e41bc8de3262475f44ec0404b4f97195599
João Sales <joaosalless@gmail.com>
2016-01-16 00:00:18 -0200

    Criando $cart - parte 4

---
+++ ionic/www/js/services/cart.js
@@ -0,0 +1,61 @@
+angular.module('starter.services')
+    .service('$cart', ['$localStorage', function ($localStorage) {
+
+        'use strict';
+
+        var key = 'cart';
+
+        this.clear = function () {
+            $localStorage.setObject(key, {
+                items: [],
+                total: 0
+            });
+        };
+
+        this.get = function () {
+            return $localStorage.getObject(key);
+        };
+
+        this.getItem = function (i) {
+            return this.get().items[i];
+        };
+
+        this.addItem = function (i) {
+            var cart = this.get(), itemAux, exists = false;
+            for (var index in cart.items) {
+                itemAux = cart.items[index];
+                if (itemAux.id == item.id) {
+                    itemAux.qtd = item.qtd + itemAux.qtd;
+                    itemAux.subtotal = calculateSubTotal(itemAux);
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) {
+                item.subtotal = calculateSubTotal(item);
+                cart.items.push(item);
+            }
+            cart.total = getTotal(cart.items);
+            $localStorage.setObject(key, cart);
+        };
+
+        this.removeItem = function (i) {
+            var cart = this.get();
+            cart.items.splice(i, 1);
+            cart.total = getTotal(cart.items);
+            $localStorage.setObject(key, cart);
+        };
+
+        function calculateSubTotal(item) {
+            return item.price * item.qtd;
+        };
+
+        function getTotal(items) {
+            var sum = 0;
+            angular.forEach(items, function (item) {
+                sum += item.subtotal;
+            });
+            return sum;
+        }
+
+    }]);
