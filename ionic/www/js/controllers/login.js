b9b6da1eac11f5add9c92d8f650d6cdb42c09165
João Sales <joaosalless@gmail.com>
2016-01-18 04:19:09 -0200

    Trabalhando com o $q

--- ionic/www/js/controllers/login.js
+++ ionic/www/js/controllers/login.js
@@ -1,15 +1,30 @@
 angular.module('starter.controllers')
     .controller('LoginController', [
-        '$scope', 'OAuth', '$state', '$ionicPopup',
-        function ($scope, OAuth, $state, $ionicPopup) {
+        '$scope', 'OAuth', '$state', '$ionicPopup', '$q',
+        function ($scope, OAuth, $state, $ionicPopup, $q) {
             'use strict';
             $scope.user = {
                 username: '',
                 password: ''
             };
 
+            function adiarExecucao() {
+                var deffered = $q.defer();
+                setTimeout(function(){
+                    deffered.resolve({name: 'ionic'});
+                });
+                return deffered.promise;
+            }
+
+            var promise = adiarExecucao()
+
             $scope.login = function () {
                 OAuth.getAccessToken($scope.user).then(function (data) {
+                    promise.then(function(data){
+                        console.log(data);
+                    }, function (dataError){
+                        console.log(data);
+                    });
                     $state.go('client.checkout');
                 }, function (responseError) {
                     $ionicPopup.alert({
