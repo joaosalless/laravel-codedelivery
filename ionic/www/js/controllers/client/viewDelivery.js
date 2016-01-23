ad1629a62351364eed64e6d3999ecd109644d298
João Sales <joaosalless@gmail.com>
2016-01-23 00:11:39 -0200

    Criando marcador de client - parte 1

--- ionic/www/js/controllers/client/viewDelivery.js
+++ ionic/www/js/controllers/client/viewDelivery.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers')
     .controller('ClientViewDeliveryController', [
-        '$scope', '$stateParams', 'ClientOrder', '$ionicLoading',
-        function ($scope, $stateParams, ClientOrder, $ionicLoading) {
+        '$scope', '$stateParams', 'ClientOrder', '$ionicLoading', '$ionicPopup', UserData,
+        function ($scope, $stateParams, ClientOrder, $ionicLoading $ionicPopup, UserData) {
             'use strict';
             $scope.order = {};
 
@@ -10,34 +10,10 @@ angular.module('starter.controllers')
                     latitude: -23.4444,
                     longitude: -46.4444
                 },
-                zoom: 16
+                zoom: 12
             };
 
-            $scope.markers = [
-                {
-                    id: 1,
-                    coords: {
-                        latitude: -23.4444,
-                        longitude: -46.4444
-                    },
-                    options: {
-                        title: 'Meu título',
-                        labelContent: 'Meu marcador',
-                        // icon: 'http://maps.google.com/mapfiles/kml/shapes/motorcycling.png'
-                    }
-                },
-                {
-                    id: 2,
-                    coords: {
-                        latitude: -22.4444,
-                        longitude: -46.4444
-                    },
-                    options: {
-                        title: 'Meu título',
-                        labelContent: 'Meu marcador'
-                    }
-                }
-            ];
+            $scope.markers = [];
 
             $ionicLoading.show({
                 template: 'Carregando...'
@@ -46,8 +22,30 @@ angular.module('starter.controllers')
             ClientOrder.get({id: $stateParams.id, include: 'items,cupom,client'}, function (data) {
                 $scope.order = data.data;
                 $ionicLoading.hide();
+                if ($scope.order.status === 1) {
+                    initMarkers();
+                } else {
+                    $ionicPopup.alert({
+                        title: 'Advertência',
+                        template: 'Pedido não está em status de entrega.'
+                    });
+                }
             }, function (dataError) {
                 $ionicLoading.hide();
             });
+
+            function initMarkers() {
+                'use strict';
+                var client = UserData.get().client,
+                    address = client.zipcode + ', ' +
+                        client.address + ', ' +
+                        client.city + ' - ' +
+                        client.state
+                createMarkerClient(address);
+            }
+
+            function createMarkerClient(address) {
+
+            }
         }
     ]);
