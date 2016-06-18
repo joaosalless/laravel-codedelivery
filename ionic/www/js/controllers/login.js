1d3bfec4011a805d7801409f15e983f20c72a95d
João Sales <joaosalless@gmail.com>
2016-06-18 09:47:13 -0300

    TouchID - parte 2

--- ionic/www/js/controllers/login.js
+++ ionic/www/js/controllers/login.js
@@ -1,14 +1,24 @@
 angular.module('starter.controllers')
-  .controller('LoginController', ['$scope', '$auth', function($scope, $auth) {
-    'use strict';
+  .controller('LoginController', [
+    '$scope', '$auth', '$cordovaTouchID',
+    function($scope, $auth, $cordovaTouchID) {
+      'use strict';
 
-    $scope.user = {
-      username: 'user@user.com',
-      password: '123456'
-    };
+      $scope.user = {
+        username: 'user@user.com',
+        password: '123456'
+      };
 
-    $scope.login = function() {
-      $auth.login($scope.user.username, $scope.user.password);
-    };
-  }
+      $scope.isSupportTouchID = false;
+
+      $scope.login = function() {
+        $auth.login($scope.user.username, $scope.user.password);
+      };
+
+      if (ionic.Platform.isWebView() && ionic.Platform.isIOS() && ionic.Platform.isIpad()) {
+        $cordovaTouchID.checkSupport().then(function() {
+          $scope.isSupportTouchID = true;
+        });
+      }
+    }
   ]);
